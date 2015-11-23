c3c6b6f4e0c2e2472179565ba05228862e905d44
Ken Vu <ken@gladeye.co.nz>
2015-11-23 16:52:48 +1300

    Implement `cli`

---
+++ cli.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+var cwd = process.cwd(),
+    path = require('path'),
+    Compiler = require('./lib/compiler'),
+    Server = require('./lib/server'),
+    meta = require('./package.json'),
+    program = require('commander'),
+    express = require('express'),
+    config;
+
+try {
+    config = require(path.join(cwd, 'sass-server.config'));
+} catch(e) {
+    config = {
+        server: null,
+        compiler: null
+    };
+}
+
+program.version(meta.version)
+    .option('-p, --port [port]', 'port number to run server on', parseInt)
+    .arguments('[dir]', 'root directory of the server')
+    .action(function(dir) {
+        if(!config.server) config.server = {};
+        config.server.root = dir;
+    })
+    .parse(process.argv);
+
+if(program.port) {
+    if(!config.server) config.server = {};
+    config.server.port = program.port;
+}
+
+
+var compiler = new Compiler(config.compiler),
+    server = new Server(compiler, config.server);
+
+server.setup();
+server.app.use(express.static(server.opts.root));
+server.run();
